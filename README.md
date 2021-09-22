b1bad08f0f92a1752bb463624791887766df7241
Thành Nhân <nhangaming125@gmail.com>
2021-09-22 23:25:30 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# General
+You will hear "Welcome to the server!" sound when joining the server
+
+# Contacts
+If you encounter an error or would like to contribute to my plugin, contact me via the platforms below:
+
+- Zalo: @thanhnhanaz
+- Gmail: nhanaz@pm.me
+- Discord: NhanAZ#9115
+- FaceBook: fb.com/thanhnhanaz
+
+# License
+[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.html)
---
+++ src/NhanAZ/CustomJoinSound/Main.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\CustomJoinSound;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\player\PlayerJoinEvent;
+
+use ReflectionClass;
+use pocketmine\resourcepacks\ZippedResourcePack;
+
+class Main extends PluginBase implements Listener
+{
+
+	public function onEnable() : void
+	{
+		$this->saveResource("CustomJoinSound.mcpack", true);
+
+		$manager = $this->getServer()->getResourcePackManager();
+		$pack = new ZippedResourcePack($this->getDataFolder() . "CustomJoinSound.mcpack");
+
+		$reflection = new ReflectionClass($manager);
+
+		$property = $reflection->getProperty("resourcePacks");
+		$property->setAccessible(true);
+
+		$currentResourcePacks = $property->getValue($manager);
+		$currentResourcePacks[] = $pack;
+		$property->setValue($manager, $currentResourcePacks);
+
+		$property = $reflection->getProperty("uuidList");
+		$property->setAccessible(true);
+		$currentUUIDPacks = $property->getValue($manager);
+		$currentUUIDPacks[strtolower($pack->getPackId())] = $pack;
+		$property->setValue($manager, $currentUUIDPacks);
+
+		$property = $reflection->getProperty("serverForceResources");
+		$property->setAccessible(true);
+		$property->setValue($manager, true);
+	}
+
+	public function onJoin(PlayerJoinEvent $event)
+	{
+		$player = $event->getPlayer();
+		$packet = new PlaySoundPacket();
+		$packet->soundName = "CustomJoinSound";
+		$packet->x = $player->getX();
+		$packet->y = $player->getY();
+		$packet->z = $player->getZ();
+		$packet->volume = 1;
+		$packet->pitch = 1;
+		$player->sendDataPacket($packet);
+	}
+}
