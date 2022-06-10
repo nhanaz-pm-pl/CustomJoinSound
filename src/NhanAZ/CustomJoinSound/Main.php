63c022009f90b5d0c6b73d462d6589d5651b6976
Restyled.io <commits@restyled.io>
2022-06-10 09:02:55 +0000

    Restyled by php-cs-fixer

--- src/NhanAZ/CustomJoinSound/Main.php
+++ src/NhanAZ/CustomJoinSound/Main.php
@@ -11,31 +11,34 @@ use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\resourcepacks\ZippedResourcePack;
 use libCustomPack\libCustomPack;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
+    private ZippedResourcePack $libRegRsp;
 
-	private ZippedResourcePack $libRegRsp;
+    protected function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        // $this->saveDefaultConfig();
+        $this->libRegRsp = libCustomPack::generatePackFromResources($plugin);
+        libCustomPack::registerResourcePack($this->libRegRsp);
+    }
 
-	protected function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		// $this->saveDefaultConfig();
-		$this->libRegRsp = libCustomPack::generatePackFromResources($plugin);
-		libCustomPack::registerResourcePack($this->libRegRsp);
-	}
+    protected function onDisable(): void
+    {
+        libCustomPack::unregisterResourcePack($this->libRegRsp);
+    }
 
-	protected function onDisable(): void {
-		libCustomPack::unregisterResourcePack($this->libRegRsp);
-	}
-
-	public function onJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer();
-		$position = $player->getPosition();
-		$packet = new PlaySoundPacket();
-		$packet->soundName = "CustomJoinSound";
-		$packet->x = $position->getX();
-		$packet->y = $position->getY();
-		$packet->z = $position->getZ();
-		$packet->volume = 1;
-		$packet->pitch = 1;
-		$player->getNetworkSession()->sendDataPacket($packet);
-	}
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $position = $player->getPosition();
+        $packet = new PlaySoundPacket();
+        $packet->soundName = "CustomJoinSound";
+        $packet->x = $position->getX();
+        $packet->y = $position->getY();
+        $packet->z = $position->getZ();
+        $packet->volume = 1;
+        $packet->pitch = 1;
+        $player->getNetworkSession()->sendDataPacket($packet);
+    }
 }
