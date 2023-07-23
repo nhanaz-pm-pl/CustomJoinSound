119542465362f5f797afc9a36fa7719fae6c923d
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-23 09:57:57 +0700

    Fixed libRegRsp not found

--- src/NhanAZ/CustomJoinSound/Main.php
+++ src/NhanAZ/CustomJoinSound/Main.php
@@ -4,23 +4,23 @@ declare(strict_types=1);
 
 namespace NhanAZ\CustomJoinSound;
 
+use NhanAZ\libBedrock\ResourcePackManager;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
-use NhanAZ\libRegRsp\libRegRsp;
 
 class Main extends PluginBase implements Listener {
 
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
-		libRegRsp::regRsp($this);
+		ResourcePackManager::registerResourcePack($this);
 	}
 
 	protected function onDisable(): void {
-		libRegRsp::unRegRsp($this);
+		ResourcePackManager::unRegisterResourcePack($this);
 	}
 
 	private function sendWelcomeSound(Player $player): void {
